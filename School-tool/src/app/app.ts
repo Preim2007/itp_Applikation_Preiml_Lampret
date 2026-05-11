c27562e9690c101dddbdb6d88007605dbde5a001
lamp <220408@studierende.htl-donaustadt.at>
2026-05-11 12:31:56 +0200

    Login und Registrierung Unterseite SCRUM 14

--- School-tool/src/app/app.ts
+++ School-tool/src/app/app.ts
@@ -3,9 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true, // Falls du Angular 17+ nutzt
+  imports: [RouterOutlet,], // Wichtig für die Links!
+  templateUrl: './app.html',      // Hier wird das HTML verknüpft
+  styleUrl: './app.css'           // HIER wird das CSS verknüpft!
 })
 export class App {
   protected readonly title = signal('School-tool');
